e754039d88b9d04c15d4a3c2c1b747bd8db64dff
Pongsakorn DataWow <pongsakorn.p@datawow.io>
2025-09-04 07:41:59 +0700

    fix: display review image

--- src/pages/qc-page/constants/columns.tsx
+++ src/pages/qc-page/constants/columns.tsx
@@ -1,68 +1,68 @@
-import { ColumnDef } from "@tanstack/react-table";
-import dayjs from "dayjs";
+import { ColumnDef } from '@tanstack/react-table';
+import dayjs from 'dayjs';
 
-import StatusBadge from "@/components/status-badge";
-import ActionButton from "../components/action-button";
+import StatusBadge from '@/components/status-badge';
+import ActionButton from '../components/action-button';
 
-import { DATE_TIME_FORMAT } from "@/contants/format";
+import { DATE_TIME_FORMAT } from '@/contants/format';
 
-import type { ReviewT } from "@/types/review";
+import type { ReviewT } from '@/types/review';
 
 export const COLUMNS: ColumnDef<ReviewT>[] = [
   {
-    accessorKey: "line_id",
-    header: "Production Line",
-    meta: { className: "text-center" },
-    cell: (info) => info.row.original.item.line_id,
+    accessorKey: 'line_id',
+    header: 'Production Line',
+    meta: { className: 'text-center' },
+    cell: info => info.row.original.item.line_id,
   },
   {
-    accessorKey: "station",
-    header: "Station",
-    meta: { className: "text-center" },
-    cell: (info) => info.row.original.item.station,
+    accessorKey: 'station',
+    header: 'Station',
+    meta: { className: 'text-center' },
+    cell: info => info.row.original.item.station,
   },
   {
-    accessorKey: "product_code",
-    header: "Product Code",
-    meta: { className: "text-center" },
-    cell: (info) => info.row.original.item.product_code,
+    accessorKey: 'product_code',
+    header: 'Product Code',
+    meta: { className: 'text-center' },
+    cell: info => info.row.original.item.product_code,
   },
   {
-    accessorKey: "roll_number",
-    header: "Roll/Bundle Number",
-    meta: { className: "text-center" },
-    cell: (info) => info.row.original.item.number,
+    accessorKey: 'roll_number',
+    header: 'Roll/Bundle Number',
+    meta: { className: 'text-center' },
+    cell: info => info.row.original.item.number,
   },
   {
-    accessorKey: "job_order_number",
-    header: "Job Order Number",
-    meta: { className: "text-center" },
-    cell: (info) => info.row.original.item.job_order_number,
+    accessorKey: 'job_order_number',
+    header: 'Job Order Number',
+    meta: { className: 'text-center' },
+    cell: info => info.row.original.item.job_order_number,
   },
   {
-    accessorKey: "detected_at",
-    header: "Timestamp",
-    meta: { className: "text-center" },
-    cell: (info) =>
+    accessorKey: 'detected_at',
+    header: 'Timestamp',
+    meta: { className: 'text-center' },
+    cell: info =>
       dayjs(info.row.original.submitted_at).format(DATE_TIME_FORMAT),
   },
   {
-    accessorKey: "status_code",
-    header: "Status",
-    meta: { className: "text-start" },
-    cell: (info) => (
+    accessorKey: 'status_code',
+    header: 'Status',
+    meta: { className: 'text-start' },
+    cell: info => (
       <StatusBadge
         status={String(info.row.original.item.status.code)}
         note={info.row.original.defects
-          .map((defect) => defect.defect_type_name)
-          .join(", ")}
+          .map(defect => defect.defect_type_name)
+          .join(', ')}
       />
     ),
   },
   {
-    accessorKey: "id",
-    header: "Action",
-    meta: { className: "text-center" },
-    cell: (info) => <ActionButton id={info.getValue<string>()} />,
+    accessorKey: 'id',
+    header: 'Action',
+    meta: { className: 'text-center' },
+    cell: info => <ActionButton id={String(info.row?.original?.item?.id)} />,
   },
 ];
