59cc6ada35e917f491636c550858aefaba83fbe8
Fadlan <fadlan.j@datawow.io>
2025-09-22 15:42:07 +0700

    fix: update useItemAPI to use FilterType for improved type safety

--- src/hooks/item/use-image.ts
+++ src/hooks/item/use-image.ts
@@ -3,7 +3,9 @@ import { useQuery } from "@tanstack/react-query";
 import { ITEM_ENDPOINT } from "@/contants/api";
 import { ItemService } from "@/services/item-service";
 
-export const useItemAPI = (params: unknown) =>
+import type { FilterType } from "@/pages/operator-page/types";
+
+export const useItemAPI = (params: FilterType) =>
   useQuery({
     queryKey: [ITEM_ENDPOINT],
     queryFn: () => ItemService.getItems(params),
