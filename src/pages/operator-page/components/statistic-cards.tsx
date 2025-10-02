6831f3134cc89097d45fdfae9b7c7cf8863ca0b5
Fadlan <fadlan.j@datawow.io>
2025-10-02 13:28:02 +0700

    refactor: update type definitions and improve layout in StatisticCards component

--- src/pages/operator-page/components/statistic-cards.tsx
+++ src/pages/operator-page/components/statistic-cards.tsx
@@ -1,11 +1,11 @@
 import { cn } from "@/lib/utils";
 
-import type { StationResponse } from "@/types/item";
+import type { ItemResponse } from "@/types/item";
 
 export default function StatisticCards({
   data,
 }: {
-  data?: StationResponse["summary"];
+  data?: ItemResponse["summary"];
 }) {
   const summary = {
     normal: 0,
@@ -26,41 +26,37 @@ export default function StatisticCards({
     <div className="flex flex-wrap gap-2">
       <div
         className={cn(
-          "min-w-[120px] p-2 border rounded text-center flex-1 border-[#B9F8CF] bg-[#DBFCE7] text-[#246630]"
+          "min-w-[120px] p-2 py-4 border rounded text-center flex-1 border-[#B9F8CF] bg-[#DBFCE7] text-[#246630] grid place-content-center"
         )}
       >
-        <p className="text-xl font-bold">{normalQC_Passed}</p>
-        <span className="text-xs">
-          จำนวนงานดีทั้งหมดในกะนี้ (Normal,QC Passed)
-        </span>
+        <p className="text-2xl font-bold">{normalQC_Passed}</p>
+        <span className="text-xs">จำนวนงานดีทั้งหมด (Normal,QC Passed)</span>
       </div>
       <div
         className={cn(
-          "min-w-[120px] p-2 border rounded text-center flex-1 min-w-[120px",
+          "min-w-[120px] p-2 border rounded text-center flex-1 grid place-content-center",
           "bg-orange-50 border-orange-200 text-orange-800"
         )}
       >
-        <p className="text-xl font-bold">{defects}</p>
-        <span className="text-xs">จำนวน Defect ทั้งหมดในกะนี้</span>
+        <p className="text-2xl font-bold">{defects}</p>
+        <span className="text-xs">จำนวน Defect ทั้งหมด</span>
       </div>
       <div
         className={cn(
-          "min-w-[120px] p-2 border rounded text-center flex-1 min-w-[120px",
+          "min-w-[120px] p-2 border rounded text-center flex-1 grid place-content-center",
           "bg-red-50 border-red-200 text-red-800"
         )}
       >
-        <p className="text-xl font-bold">{scrap}</p>
-        <span className="text-xs">
-          จำนวนของเสียทั้งหมดในกะนี้ (Scrap,Rejected)
-        </span>
+        <p className="text-2xl font-bold">{scrap}</p>
+        <span className="text-xs">จำนวนของเสียทั้งหมด (Scrap,Rejected)</span>
       </div>
       <div
         className={cn(
-          "min-w-[120px] p-2 border rounded text-center flex-1 min-w-[120px",
+          "min-w-[120px] p-2 border rounded text-center flex-1 grid place-content-center",
           "bg-orange-100 border-orange-300 text-orange-700"
         )}
       >
-        <p className="text-xl font-bold">{pending_defect}</p>
+        <p className="text-2xl font-bold">{pending_defect}</p>
         <span className="text-xs">จำนวน Defect ที่รอการตรวจสอบ</span>
       </div>
     </div>
