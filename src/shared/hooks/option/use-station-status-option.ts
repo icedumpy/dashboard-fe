b4d90fcdff1a35ed241a069217fb88aca552761a
Fadlan <fadlan.j@datawow.io>
2025-10-08 09:35:56 +0700

    refactor: replace hardcoded status options with API integration for dynamic retrieval

--- src/shared/hooks/option/use-station-status-option.ts
+++ src/shared/hooks/option/use-station-status-option.ts
@@ -1,32 +1,14 @@
-import { STATUS } from "@/shared/constants/status";
+import { useGetItemStatusAPI } from "../item-status/use-get-item-status";
 
 import type { OptionT } from "@/shared/types/option";
 
 export const useStationStatusOptions = (): OptionT[] => {
-  return [
-    {
-      label: STATUS.DEFECT.toLocaleUpperCase(),
-      value: STATUS.DEFECT,
-    },
-    {
-      label: STATUS.REJECTED.toLocaleUpperCase(),
-      value: STATUS.REJECTED,
-    },
-    {
-      label: STATUS.SCRAP.toLocaleUpperCase(),
-      value: STATUS.SCRAP,
-    },
-    {
-      label: STATUS.RECHECK.toLocaleUpperCase(),
-      value: STATUS.RECHECK,
-    },
-    {
-      label: STATUS.NORMAL.toLocaleUpperCase(),
-      value: STATUS.NORMAL,
-    },
-    {
-      label: STATUS.QC_PASSED.toLocaleUpperCase(),
-      value: STATUS.QC_PASSED,
-    },
-  ];
+  const { data: statuses } = useGetItemStatusAPI();
+
+  return (
+    statuses?.data?.map((status) => ({
+      label: status.code.toLocaleUpperCase(),
+      value: status.code,
+    })) || []
+  );
 };
